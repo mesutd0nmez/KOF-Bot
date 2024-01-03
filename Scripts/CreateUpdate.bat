ba84251e59bc629bdfe5c27b6ea40f2f9c789777
Türkay Tanrikulu <trky.shorty@gmail.com>
2024-01-03 11:30:39 +0300

    Remove unused files

--- Scripts/CreateUpdate.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-del Update.zip
-powershell Compress-Archive -Path ..\Deployment\DSAServiceHelper.exe -DestinationPath ..\Deployment\Update.zip -Force
-echo Created Update.zip
--- Scripts/Deploy.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-xcopy /y ..\Production\Adapter.dll ..\Deployment
-xcopy /y ..\Production\Updater.exe ..\Deployment
-xcopy /y ..\Production\KOF.Checksum.exe ..\Deployment
