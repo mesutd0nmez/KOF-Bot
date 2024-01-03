78920f930ddc9fe76e22169849c24f5250a7deba
Türkay Tanrikulu <trky.shorty@gmail.com>
2024-01-03 09:30:37 +0300

    New definitions was added and definitions were made according to debug and release.

--- KOF.Bot/Define.h
+++ KOF.Bot/Define.h
@@ -1,60 +1,49 @@
 #pragma once
 
-///////////////// DEBUG ////////////////////
+#define INJECT_PATH "C:\\NTTGame\\KnightOnlineEn"
+#define INJECT_EXE "KnightOnLine.exe"
+#define INJECT_PLATFORM 0
+
+//#define GAME_PATH "C:\\CNKO"
+//#define GAME_EXE "KnightOnLine.exe"
+//#define GAME_PLATFORM 1
+
+//#define GAME_PATH "C:\\Ongate\\KnightOnline"
+//#define GAME_EXE "knightonline.exe"
+//#define GAME_PLATFORM 2
+//#define GAME_ACCOUNT_INDEX 1
+
+//#define GAME_PATH "C:\\Program Files (x86)\\Steam\\steamapps\\common\\Knight Online"
+//#define GAME_EXE "KnightOnLine.exe"
+//#define GAME_PLATFORM 3
 
 #ifdef _DEBUG
 #define DEBUG
+#define DEBUG_LOG
+//#define PRINT_PACKET
 #else
-//#define DEBUG 
+#define ENABLE_TRIGGER_BSOD
+#define ENABLE_ANTI_DEBUG
+#define ENABLE_TRAP_CHECK
+#define ENABLE_PONG_TIMEOUT_CHECK
+#define ENABLE_SERVER_CONNECTION_LOST_CHECK
+#define ENABLE_ANTI_SUSPEND_PROTECT
+#define ENABLE_INTERACTIVE_CHECK
+#define ENABLE_REPORT_WITH_SCREENSHOT
 #endif
 
-#ifdef DEBUG
+//#define ENABLE_MAIN_PERFORMANCE_COUNTER
+//#define ENABLE_BOT_PERFORMANCE_COUNTER
+//#define ENABLE_HANDLER_PERFORMANCE_COUNTER
+
+#ifdef PRINT_PACKET
 #define PRINT_RECV_PACKET
 #define PRINT_SEND_PACKET
 #endif
 
-#define DISABLE_XIGNCODE
+//#define DISABLE_XIGNCODE
 
-///////////////// PROTECTION ////////////////////
-//#define ENABLE_ATTACH_PROTECT
-//#define ENABLE_SUSPEND_PROTECT
-//#define ENABLE_SERVER_CONNECTION_LOST_PROTECT
-//#define ENABLE_DEBUGGER_PRESENT_PROTECT
-//#define ENABLE_PONG_TIMEOUT_PROTECT
 
-///////////////// IMPORTANT ////////////////////
-
-#define DEVELOPMENT_PATH "C:\\NTTGame\\KnightOnlineEn"
-#define DEVELOPMENT_EXE "KnightOnLine.exe"
-#define DEVELOPMENT_PLATFORM 0
-
-///////////////// FEATURES /////////////////
-
-//#define FEATURE_LEVEL_DOWNER
-#define FEATURE_TOURNAMENT_SKILLS
-
-//#define DEVELOPMENT_PATH "C:\\CNKO"
-//#define DEVELOPMENT_EXE "KnightOnLine.exe"
-//#define DEVELOPMENT_PLATFORM 1
-//#define DEVELOPMENT_ACCOUNT_INDEX 9
-
-//#define DEVELOPMENT_PATH "C:\\Ongate\\KnightOnline"
-//#define DEVELOPMENT_EXE "knightonline.exe"
-//#define DEVELOPMENT_PLATFORM 3
-//#define DEVELOPMENT_ACCOUNT_INDEX 1
-
-//#define DEVELOPMENT_PATH "C:\\Program Files (x86)\\Steam\\steamapps\\common\\Knight Online"
-//#define DEVELOPMENT_EXE "KnightOnLine.exe"
-//#define DEVELOPMENT_PLATFORM 4
-//#define DEVELOPMENT_ACCOUNT_INDEX 11
-
-///////////////// UI ///////////////////////
-
-//#define UI_HAZAR
-//#define UI_BLANK
-//#define UI_DEFAULT
-//#define UI_COMMON
-//#define DEVELOPER_ONLY
 
 
 
