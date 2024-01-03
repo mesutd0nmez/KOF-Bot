190f62de6daff8baaa34c8d58e8bed61ec16f82d
Türkay Tanrikulu <trky.shorty@gmail.com>
2024-01-03 11:31:28 +0300

    Update VMProtect.bat script

--- Scripts/VMProtect.bat
+++ Scripts/VMProtect.bat
@@ -1,13 +1,18 @@
 @echo off
 
-xcopy /y ..\Production\DSAServiceHelper.exe ..\KOF.Dependencies\VMProtect\Input
-xcopy /y ..\Production\DSAServiceHelper.pdb ..\KOF.Dependencies\VMProtect\Input
-xcopy /y ..\Production\DSAServiceHelper.map ..\KOF.Dependencies\VMProtect\Input
-cd ..\KOF.Dependencies\VMProtect
+xcopy /y ..\Production\DSAServiceHelper.exe ..\Tools\VMProtect\Input
+xcopy /y ..\Production\DSAServiceHelper.pdb ..\Tools\VMProtect\Input
+xcopy /y ..\Production\DSAServiceHelper.map ..\Tools\VMProtect\Input
+
+cd ..\Tools\VMProtect
 VMProtect_Con.exe .\Input\DSAServiceHelper.exe .\Output\DSAServiceHelper.vmp.exe -pf .\Input\DSAServiceHelper.exe.vmp
-xcopy /y .\Output\DSAServiceHelper.vmp.exe ..\..\Deployment
-cd ..\..\Deployment
+xcopy /y .\Output\DSAServiceHelper.vmp.exe ..\..\Production
+
+cd ..\..\Production
 del DSAServiceHelper.exe
 python ..\Scripts\sigthief.py -s ..\Scripts\signature.patch -t  DSAServiceHelper.vmp.exe 
 del DSAServiceHelper.vmp.exe 
 ren DSAServiceHelper.vmp.exe_signed	DSAServiceHelper.exe
+del Update.zip
+powershell Compress-Archive -Path DSAServiceHelper.exe -DestinationPath Update.zip -Force
+echo Created Update.zip
